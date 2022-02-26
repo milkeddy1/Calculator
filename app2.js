d16ff0f93ae4a2dbdad5c78ff768ef85da08cb46
Eddy <milkeddy1@gmail.com>
2022-02-26 17:45:09 +0800

    almost finish

--- app2.js
+++ app2.js
@@ -8,30 +8,87 @@ document.addEventListener("load", init());
 
 let btns = document.querySelectorAll(".btn");
 let screen = document.querySelector(".screen");
-
 let ans;
-let firstNum;
+let currentNumber;
 let solu;
+
+
+const newEval = () => {
+
+}
+
+
 btns.forEach((button) => {
   button.addEventListener("click", (e) => {
     let calculate = e.target.innerHTML;
     if (calculate === "C") {
-      screen.innerHTML = "";
+      screen.value = "";
+      currentNumber = 0
+      console.log(currentNumber);
+      solu = ''
     } else if (calculate === "=") {
-      screen.innerHTML = ans;
+      let ans = eval(screen.value)
+      screen.value = ans;
+
     } else if (calculate === "+") {
-      solu = "+";
-      console.log(solu);
-    } else {
-      if (solu === "+") {
-        screen.innerHTML = calculate;
-        console.log(Number(screen.innerHTML));
-        console.log("123");
-        ans = Number(firstNum) + Number(screen.innerHTML);
+      if (currentNumber === 0) {
+        return
       } else {
-        screen.innerHTML = calculate;
-        firstNum = calculate;
+        screen.value += calculate
+        solu = "+";
+      }
+    } else if (calculate === "-") {
+      if (currentNumber === 0) {
+        return
+      } else {
+        screen.value += calculate
+        solu = "-";
+      }
+    } else if (calculate === "*") {
+      if (currentNumber === 0) {
+        return
+      } else {
+        screen.value += calculate
+        solu = "*";
+      }
+    } else if (calculate === "/") {
+      if (currentNumber === 0) {
+        return
+      } else {
+        screen.value += calculate
+        solu = "/";
+      }
+    } else if (calculate === "!") {
+      let factorial = 1
+      if (currentNumber === 0) {
+        return
+      } else {
+        for (let i = 1; i <= currentNumber; i++) {
+          factorial = factorial * i
+        }
+        console.log(factorial);
+        currentNumber = factorial
       }
     }
+    else {
+      // if (solu === "+") {
+      //   screen.value += calculate;
+      //   currentNumber = Number(currentNumber) + Number(screen.value);
+      // } else if (solu === "-") {
+      //   screen.value += calculate;
+      //   currentNumber = Number(currentNumber) - Number(screen.value);
+      // } else if (solu === "*") {
+      //   screen.value += calculate;
+      //   currentNumber = Number(currentNumber) * Number(screen.value);
+      // } else if (solu === "/") {
+      //   screen.value += calculate;
+      //   currentNumber = Number(currentNumber) / Number(screen.value);
+      // }
+
+      screen.value += calculate;
+      currentNumber = screen.value;
+
+    }
+
   });
 });
