e425887e663ba0848a6c33f593853fd2dfa5f281
Chia Wei Liao <chiaweiliao@ChiadeMacBook-Air.local>
2022-02-27 11:28:16 +0800

    fix

--- app2.js
+++ app2.js
@@ -8,87 +8,74 @@ document.addEventListener("load", init());
 
 let btns = document.querySelectorAll(".btn");
 let screen = document.querySelector(".screen");
-let ans;
+let ans = 0;
 let currentNumber;
 let solu;
+let formula = "";
+let ans2 = 0;
 
-
-const newEval = () => {
-
-}
-
+const newEval = () => {};
 
 btns.forEach((button) => {
   button.addEventListener("click", (e) => {
-    let calculate = e.target.innerHTML;
+    let calculate = e.target.dataset.id;
     if (calculate === "C") {
       screen.value = "";
-      currentNumber = 0
-      console.log(currentNumber);
-      solu = ''
+      currentNumber = 0;
+      formula = "";
     } else if (calculate === "=") {
-      let ans = eval(screen.value)
+      let ans = eval(formula);
+      formula = "";
       screen.value = ans;
-
     } else if (calculate === "+") {
       if (currentNumber === 0) {
-        return
+        return;
       } else {
-        screen.value += calculate
-        solu = "+";
+        screen.value = "";
+        formula += calculate;
+        currentNumber = 0;
       }
     } else if (calculate === "-") {
       if (currentNumber === 0) {
-        return
+        return;
       } else {
-        screen.value += calculate
-        solu = "-";
+        formula = screen.value;
+        screen.value = "";
+        formula += calculate;
       }
     } else if (calculate === "*") {
       if (currentNumber === 0) {
-        return
+        return;
       } else {
-        screen.value += calculate
-        solu = "*";
+        formula = screen.value;
+        screen.value = "";
+        formula += calculate;
       }
     } else if (calculate === "/") {
       if (currentNumber === 0) {
-        return
+        return;
       } else {
-        screen.value += calculate
-        solu = "/";
+        formula = screen.value;
+        screen.value = "";
+        formula += calculate;
       }
     } else if (calculate === "!") {
-      let factorial = 1
+      let factorial = 1;
+      currentNumber = screen.value;
       if (currentNumber === 0) {
-        return
+        return;
       } else {
         for (let i = 1; i <= currentNumber; i++) {
-          factorial = factorial * i
+          factorial = factorial * i;
         }
-        console.log(factorial);
-        currentNumber = factorial
+        screen.value = "";
+        formula += factorial;
+        formula = formula.replace(currentNumber, "");
       }
-    }
-    else {
-      // if (solu === "+") {
-      //   screen.value += calculate;
-      //   currentNumber = Number(currentNumber) + Number(screen.value);
-      // } else if (solu === "-") {
-      //   screen.value += calculate;
-      //   currentNumber = Number(currentNumber) - Number(screen.value);
-      // } else if (solu === "*") {
-      //   screen.value += calculate;
-      //   currentNumber = Number(currentNumber) * Number(screen.value);
-      // } else if (solu === "/") {
-      //   screen.value += calculate;
-      //   currentNumber = Number(currentNumber) / Number(screen.value);
-      // }
-
+    } else {
+      currentNumber = calculate;
+      formula += calculate;
       screen.value += calculate;
-      currentNumber = screen.value;
-
     }
-
   });
 });
